2866203bbceba8bdade76b0a2e3ad0346870c654
GowthamAravind <35756844+GowthamAravind@users.noreply.github.com>
2018-03-03 14:32:01 +0530

    Update and rename .gitignore to vin.java

---
+++ .gitignore/vin.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String a="abc";
+		char v[]=a.toCharArray();
+		for(int i=a.length()-1;i>=0;i--)
+			System.out.print(v[i]);
+		
+	}
+}
