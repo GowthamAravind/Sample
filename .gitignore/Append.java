7ec96fd4aed6019370ee390d7c8e2547182598df
GowthamAravind <35756844+GowthamAravind@users.noreply.github.com>
2018-03-05 14:40:01 +0530

    Create Append.java

---
+++ .gitignore/Append.java
@@ -0,0 +1,15 @@
+public class F {
+static char temp;
+	public static void main(String[] args) {
+			StringBuffer a=new StringBuffer("shashank have");
+			
+			a.append(".");
+			
+			System.out.print(a);
+			
+		    
+
+
+	}
+
+}
