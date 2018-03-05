25f7b2704027738a494be8626959c6ae99cebd49
GowthamAravind <35756844+GowthamAravind@users.noreply.github.com>
2018-03-05 14:03:11 +0530

    Update Nre.java

--- .gitignore/Nre.java
+++ .gitignore/Nre.java
@@ -3,13 +3,14 @@ class Nre
 {
 Scanner in=new Scanner(System.in)
 int n=in.nextInt();
-int a[]=new int[n];
-for(int i=0;i<n;i++)
-{
-int v=in.nextInt();
-a[i]=v;
-}
-for(int i=a.length;i>=0;i--)
-{
-System.out.println(a[i]);
-}
+int h;
+  int re=0;
+  int sum=0;
+  h=n;
+  while(n!=0)
+  {
+    re=n%10;
+    sum=sum*10+re;
+    n/=10;
+  }
+  System.out.println(sum)
